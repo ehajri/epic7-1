bf78f824c12b0e4a46748bac3ff3239e3d1a9ef9
Ebrahem Al-Hajeri <ebrahem.alhajri@gmail.com>
2020-02-04 06:27:34 +0300

    still working

--- script.py
+++ script.py
@@ -9,6 +9,7 @@ import numpy as np
 import random
 from functions import *
 from matplotlib import pyplot as plt
+from verification import gear
 
 resolution = "1600"
 files = [
@@ -24,61 +25,89 @@ def draw(img, xy=None):
         cv2.rectangle(img, (xy[1][0], xy[0][0]), (xy[1][1], xy[0][1]), (255, 0, 0), 2)
     plt.subplot(1,1,1), plt.imshow(img), plt.show()
 
-for f in files:
-    for m in marks:
-        mark = cv2.imread(m, 0)
-        imgg = cv2.imread(f)
-        res = cv2.matchTemplate(cv2.cvtColor(imgg, cv2.COLOR_BGR2GRAY), mark, cv2.TM_CCOEFF_NORMED)
-        minv, maxv, min_loc, max_loc = cv2.minMaxLoc(res)
-        if maxv < 0.9:
-            print("Not found", m)
-        else:
-            # debugging
-            TB_H = coords["TOP_BOX"]["HEIGHT"]
-            TB_W = coords["TOP_BOX"]["WIDTH"]
-            y1 = max_loc[1]
-            y2 = max_loc[1] + TB_H
-            x1 = max_loc[0] - TB_W
-            x2 = max_loc[0]
 
-            top_box = imgg[y1:y2, x1:x2]
-            top_coords = coords["TOP"]
 
-            export = {}
-            for k in top_coords:
-                crds = top_coords[k]
-                data = process(k, top_box[crds[0][0]:crds[0][1], crds[1][0]:crds[1][1]])
-                if k == 'LVL':
-                    export["ilvl"] = digit_filter(data.replace('S', '5').replace('B', '8').replace('a', '8'))
+def analyze(img, debug=False):
+    # img is an array
+    mark = cv2.imread(coords['MARKS']['TOP'], 0)
+    res = cv2.matchTemplate(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), mark, cv2.TM_CCOEFF_NORMED)
+    minv, maxv, min_loc, max_loc = cv2.minMaxLoc(res)
+    if maxv < 0.9:
+        print("Not found")
+        return
+    TB_H = coords["TOP_BOX"]["HEIGHT"]
+    TB_W = coords["TOP_BOX"]["WIDTH"]
+    y1 = max_loc[1]
+    y2 = max_loc[1] + TB_H
+    x1 = max_loc[0] - TB_W
+    x2 = max_loc[0]
 
-                if k == 'PLUS':
-                    export["enhance"] = digit_filter(data.replace('S', '5').replace('B', '8').replace('a', '8').replace('19', '15'))
+    top_box = img[y1:y2, x1:x2]
+    top_coords = coords["TOP"]
 
-                if k == 'TYPE':
-                    export["grade"] = char_filter(data.split(' ')[0])
-                    export["slot"] = char_filter(data.split(' ')[1].split('\n')[0])
+    export = {}
+    for k in top_coords:
+        crds = top_coords[k]
+        data = process(k, top_box[crds[0][0]:crds[0][1], crds[1][0]:crds[1][1]])
+        if k == 'LVL':
+            export["ilvl"] = digit_filter(data.replace('S', '5').replace('B', '8').replace('a', '8'))
 
-            BOTTOM_X = x2 - coords["TOP_BOX"]["WIDTH"]
+        if k == 'PLUS':
+            export["enhance"] = digit_filter(
+                data.replace('S', '5').replace('B', '8').replace('a', '8').replace('19', '15'))
 
-            temp_bot = cv2.imread("e7/bottom2.png", 0)
-            _, _, _, max_loc = cv2.minMaxLoc(
-                cv2.matchTemplate(cv2.cvtColor(imgg, cv2.COLOR_BGR2GRAY), temp_bot, cv2.TM_CCOEFF_NORMED))
+        if k == 'TYPE':
+            if debug: draw(top_box, crds)
+            export["grade"] = char_filter(data.split(' ')[0])
+            export["slot"] = char_filter(data.split(' ')[1].split('\n')[0])
 
-            BB = coords["BOTTOM_BOX"]
-            bottom_box = imgg[max_loc[1] + BB['SHIFT']:max_loc[1] + BB['HEIGHT'] + BB['SHIFT'], BOTTOM_X:BOTTOM_X + BB['WIDTH']]
+    BOTTOM_X = x2 - coords["TOP_BOX"]["WIDTH"]
 
-            bottom_coords = coords["BOTTOM"]
+    temp_bot = cv2.imread(coords['MARKS']['BOTTOM'], 0)
+    _, _, _, max_loc = cv2.minMaxLoc(
+        cv2.matchTemplate(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), temp_bot, cv2.TM_CCOEFF_NORMED))
 
-            for k in bottom_coords:
-                crds = bottom_coords[k]
-                data = process(k, bottom_box[crds[0][0]:crds[0][1], crds[1][0]:crds[1][1]])
-                if k == 'MAIN':
-                    export["main"] = {'stat': stat_converter(data), 'value': digit_filter(data)}
-                if k == 'SUBS':
-                    export["subs"] = []
-                    for n, entry in enumerate(data.split('\n')):
-                        export['subs'].append({'stat': stat_converter(entry), 'value': digit_filter(entry.replace('T%', '7%'))})
-                if k == 'SET':
-                    export['set'] = char_filter(data.split(' Set')[0])
-            print(export)
+    BB = coords["BOTTOM_BOX"]
+    bottom_box = img[max_loc[1] + BB['SHIFT']:max_loc[1] + BB['HEIGHT'] + BB['SHIFT'], BOTTOM_X:BOTTOM_X + BB['WIDTH']]
+
+    bottom_coords = coords["BOTTOM"]
+
+    for k in bottom_coords:
+        crds = bottom_coords[k]
+        data = process(k, bottom_box[crds[0][0]:crds[0][1], crds[1][0]:crds[1][1]])
+        if k == 'MAIN':
+            export["main"] = {'stat': stat_converter(data), 'value': digit_filter(data)}
+        if k == 'SUBS':
+            export["subs"] = []
+            for n, entry in enumerate(data.split('\n')):
+                export['subs'].append({'stat': stat_converter(entry), 'value': digit_filter(entry.replace('T%', '7%'))})
+        if k == 'SET':
+            export['set'] = char_filter(data.split(' Set')[0])
+    return export
+
+files = ['ss8.png', 'ss10.png', 'ss11.png', 'ss12.png', 'ss13.png', 'ss14.png', 'ss15.png', 'ss16.png', 'ss17.png']
+from jsondiff import diff
+for file in files:
+    if file != 'ss11.png': continue
+    if file == 'ss11.png':
+        e = analyze(cv2.imread("screenshots/"+file), True)
+    e = analyze(cv2.imread("screenshots/" + file))
+    v = gear[file]
+    r = e == v
+    if r:
+        print(file, e==v)
+    else:
+        print(file, "diff:", e)
+
+# for f in files:
+#     for m in marks:
+#         mark = cv2.imread(m, 0)
+#         imgg = cv2.imread(f)
+#         res = cv2.matchTemplate(cv2.cvtColor(imgg, cv2.COLOR_BGR2GRAY), mark, cv2.TM_CCOEFF_NORMED)
+#         minv, maxv, min_loc, max_loc = cv2.minMaxLoc(res)
+#         if maxv < 0.9:
+#             print("Not found", m)
+#         else:
+#             # debugging
+#             analyze(cv2.imread(f))
 
---
+++ verification.py
@@ -0,0 +1,84 @@
+
+gear = {
+    "ss8.png": {
+        'ilvl': 85, 'enhance': 15, 'grade': 'Epic', 'slot': 'Weapon',
+        'main': {'stat': 'Atk', 'value': 500},
+        'subs': [{'stat': 'Spd', 'value': 3},
+                 {'stat': 'Eff', 'value': 13},
+                 {'stat': 'CChance', 'value': 9},
+                 {'stat': 'Res', 'value': 22}],
+        'set': 'Speed'
+    },
+    "ss10.png": {
+        'ilvl': 85, 'enhance': 15, 'grade': 'Epic', 'slot': 'Weapon',
+        'main': {'stat': 'Atk', 'value': 500},
+        'subs': [{'stat': 'CChance', 'value': 9},
+                 {'stat': 'AtkP', 'value': 13},
+                 {'stat': 'HPP', 'value': 11},
+                 {'stat': 'CDmg', 'value': 16}],
+        'set': 'Speed'
+    },
+    "ss11.png": {
+        'ilvl': 88, 'enhance': 15, 'grade': 'Epic', 'slot': 'Ring',
+        'main': {'stat': 'Eff', 'value': 65},
+        'subs': [{'stat': 'HPP', 'value': 18},
+                 {'stat': 'AtkP', 'value': 12},
+                 {'stat': 'Spd', 'value': 8},
+                 {'stat': 'CChance', 'value': 8}],
+        'set': 'Speed'
+    },
+    "ss12.png": {
+        'ilvl': 71, 'enhance': 15, 'grade': 'Epic', 'slot': 'Armor',
+        'main': {'stat': 'Def', 'value': 260},
+        'subs': [{'stat': 'HPP', 'value': 15},
+                 {'stat': 'Spd', 'value': 6},
+                 {'stat': 'CChance', 'value': 2},
+                 {'stat': 'Eff', 'value': 10}],
+        'set': 'Lifesteal'
+    },
+    "ss13.png": {
+        'ilvl': 85, 'enhance': 14, 'grade': 'Heroic', 'slot': 'Weapon',
+        'main': {'stat': 'Atk', 'value': 420},
+        'subs': [{'stat': 'Eff', 'value': 12},
+                 {'stat': 'Res', 'value': 16},
+                 {'stat': 'Spd', 'value': 2},
+                 {'stat': 'AtkP', 'value': 4}],
+        'set': 'Defense'
+    },
+    "ss14.png": {
+        'ilvl': 88, 'enhance': 15, 'grade': 'Epic', 'slot': 'Helmet',
+        'main': {'stat': 'HP', 'value': 2765},
+        'subs': [{'stat': 'AtkP', 'value': 17},
+                 {'stat': 'Spd', 'value': 8},
+                 {'stat': 'Res', 'value': 5},
+                 {'stat': 'Eff', 'value': 10}],
+        'set': 'Attack'
+    },
+    "ss15.png": {
+        'ilvl': 85, 'enhance': 15, 'grade': 'Heroic', 'slot': 'Helmet',
+        'main': {'stat': 'HP', 'value': 2700},
+        'subs': [{'stat': 'DefP', 'value': 17},
+                 {'stat': 'CChance', 'value': 8},
+                 {'stat': 'AtkP', 'value': 13},
+                 {'stat': 'Def', 'value': 29}],
+        'set': 'Destruction'
+    },
+    "ss16.png": {
+        'ilvl': 85, 'enhance': 15, 'grade': 'Epic', 'slot': 'Armor',
+        'main': {'stat': 'Def', 'value': 300},
+        'subs': [{'stat': 'CChance', 'value': 18},
+                 {'stat': 'Eff', 'value': 12},
+                 {'stat': 'HPP', 'value': 8},
+                 {'stat': 'CDmg', 'value': 4}],
+        'set': 'Health'
+    },
+    "ss17.png": {
+        'ilvl': 71, 'enhance': 15, 'grade': 'Epic', 'slot': 'Armor',
+        'main': {'stat': 'Def', 'value': 260},
+        'subs': [{'stat': 'HPP', 'value': 19},
+                 {'stat': 'DefP', 'value': 9},
+                 {'stat': 'Spd', 'value': 3},
+                 {'stat': 'Res', 'value': 9}],
+        'set': 'Speed'
+    },
+}
